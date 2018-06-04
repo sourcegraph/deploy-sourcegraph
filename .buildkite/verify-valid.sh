95354f440f28c3c4be5ab794cd5911f26efa950d
Beyang Liu <beyang@sourcegraph.com>
2018-06-04 15:19:17 -0700

    remove `test` script and add test-cases/custom-ls-address

--- .buildkite/verify-valid.sh
+++ .buildkite/verify-valid.sh
@@ -8,3 +8,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 # Can't use xargs since kubeval ignores args if stdin is not a tty. xargs in
 # busybox doesn't support -o to make stdin a tty.
 kubeval $(find examples -path '*/generated/*.yaml')
+kubeval $(find test-cases -path '*/generated/*.yaml')
