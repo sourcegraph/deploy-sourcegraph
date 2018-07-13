95fd8d5c1f5d4894e8169a180207e98ffa29c872
Nick Snyder <nick@sourcegraph.com>
2018-07-13 15:25:26 -0700

    buildkite

---
+++ .buildkite/install-kustomize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION=1.0.3
+
+if [ ! $(which kustomize) ]; then
+    echo "Installing kustomize to /usr/local/bin"
+    wget https://github.com/kubernetes-sigs/kustomize/releases/download/v${VERSION}/kustomize_${VERSION}_linux_amd64 -O /user/local/bin
+fi
