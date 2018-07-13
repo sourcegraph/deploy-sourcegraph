f24c2e8cc22a0fd36b4376e95d3b1a6d32e4495c
Nick Snyder <nick@sourcegraph.com>
2018-07-13 15:32:08 -0700

    fix permission

--- .buildkite/install-kustomize.sh
+++ .buildkite/install-kustomize.sh
@@ -5,4 +5,5 @@ VERSION=1.0.3
 if [ ! $(which kustomize) ]; then
     echo "Installing kustomize to /usr/local/bin"
     wget https://github.com/kubernetes-sigs/kustomize/releases/download/v${VERSION}/kustomize_${VERSION}_linux_amd64 -O /usr/local/bin/kustomize
+    chmod a+x /usr/local/bin/kustomize
 fi
