8f67538436886ec2dbe53a1c5e78994432330168
Nick Snyder <nick@sourcegraph.com>
2018-08-17 21:12:44 -0700

    fix build

--- .buildkite/install-jy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION=1.0.0
-
-if [ ! $(which jy) ]; then
-    echo "Installing jy to /usr/local/bin"
-    wget https://github.com/sourcegraph/jy/releases/download/v${VERSION}/jy-${VERSION}-linux-amd64 -O /usr/local/bin/jy
-    chmod a+x /usr/local/bin/jy
-fi
--- .buildkite/verify-label.sh
+++ .buildkite/verify-label.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-if find base configure -name "*.yaml" -exec sh -c "cat {} | yj | jq --raw-output '.metadata.labels.deploy'" \; | grep -v sourcegraph; then
-    echo "> There exists a yaml file in base/ or configure/ that does not contain .metadata.labels.deploy == sourcegraph"
+.buildkite/install-yj.sh
+
+if find base -name "*.yaml" -exec sh -c "cat {} | yj | jq --raw-output '.metadata.labels.deploy'" \; | tee /tmp/deploy-label | grep -v sourcegraph; then
+    echo "> There exists a yaml file in base/ that does not contain .metadata.labels.deploy == sourcegraph"
     echo "> Run the following command to fix:"
-    echo "find base/ configure/ -name \"*.yaml\" -exec sh -c \"cat {} | yj | jq '.metadata.labels.deploy = \\\"sourcegraph\\\"' | jy -o {}\" \;"
+    echo "find base/ -name \"*.yaml\" -exec sh -c \"cat {} | yj | jq '.metadata.labels.deploy = \\\"sourcegraph\\\"' | jy -o {}\" \;"
 
     exit 1
 fi
