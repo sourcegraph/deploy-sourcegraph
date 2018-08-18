8d3c4aaebb43ca89ca7ab8f99e82803c18eb3ae0
Nick Snyder <nick@sourcegraph.com>
2018-08-17 21:26:41 -0700

    parallel

--- .buildkite/verify-yaml.sh
+++ .buildkite/verify-yaml.sh
@@ -4,9 +4,8 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 .buildkite/install-kubeval.sh
-.buildkite/verify-label.sh
 
-# Validate base yaml
 find base -name '*.yaml' -exec kubeval {} +
+find configure -name '*.yaml' -exec kubeval {} +
 
 .buildkite/verify-label.sh
