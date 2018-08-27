3dfd58e95242de92cc463a4840cb143cb67e4fed
Nick Snyder <nick@sourcegraph.com>
2018-08-27 16:03:29 -0600

    simplify wording

--- configure/kubectl-apply-all.sh
+++ configure/kubectl-apply-all.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 #
-# This file should be filled in with simple `kubectl apply` commands 
-# that will apply the YAML configuration files for each resource that 
-# are deployed / you want to be deployed to your cluster. 
+# This file should contain `kubectl apply` commands for all of your configured resources.
 # 
 # This file should be run:
 #   * When the cluster is first created
 #   * Whenever the configuration for any resource has been updated
-
 
 kubectl apply --prune -l deploy=sourcegraph -f base --recursive
