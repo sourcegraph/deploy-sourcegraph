441c993186879d319362b05017f36c8ce2bba9a1
Nick Snyder <nickdsnyder@gmail.com>
2018-09-01 14:39:11 -0700

    more validate (#77)

--- .buildkite/verify-yaml.sh
+++ .buildkite/verify-yaml.sh
@@ -5,6 +5,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 .buildkite/install-kubeval.sh
 
+kubectl apply --dry-run --validate --recursive -f base/
+kubectl apply --dry-run --validate --recursive -f configure/
+
 find base -name '*.yaml' -exec kubeval {} +
 find configure -name '*.yaml' -exec kubeval {} +
 
