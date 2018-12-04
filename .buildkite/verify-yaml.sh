4847a6bca7c541432a0e37290a325f4d2d814a8a
Geoffrey Gilmore <geoffrey@sourcegraph.com>
2018-12-04 11:25:01 -0800

    buildkite: change name of dogfood cluster

--- .buildkite/verify-yaml.sh
+++ .buildkite/verify-yaml.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 .buildkite/install-kubeval.sh
 
-gcloud container clusters get-credentials dogfood-cluster-7 --zone us-central1-a --project sourcegraph-dev
+gcloud container clusters get-credentials dogfood --zone us-central1-a --project sourcegraph-dev
 
 kubectl apply --dry-run --validate --recursive -f base/
 kubectl apply --dry-run --validate --recursive -f configure/
