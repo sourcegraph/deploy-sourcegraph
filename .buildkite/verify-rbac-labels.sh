7eacf6bc6b03b1da7f10b6bdac9da6d20c78902d
ggilmore <geoffrey@sourcegraph.com>
2019-02-22 13:06:53 -0800

    use install script

--- .buildkite/verify-rbac-labels.sh
+++ .buildkite/verify-rbac-labels.sh
@@ -4,6 +4,9 @@ set -ex
 
 cd $(dirname "${BASH_SOURCE[0]}")/..
 
+
+.buildkite/install-yaml2json.sh
+
 missing_labels=$(find base -name "*.yaml" -print0 | xargs -0L1 -I {} sh -c "cat {} | yaml2json | jq --exit-status -f .buildkite/rbac.jq > /dev/null || echo {}")
 
 if [ ! -z "${missing_labels}" ]; then
