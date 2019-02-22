2341a03da7a77d33f98d0166b641769fe1b9d0ae
ggilmore <geoffrey@sourcegraph.com>
2019-02-22 13:04:40 -0800

    ci: verify rbac labels script

---
+++ .buildkite/verify-rbac-labels.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+cd $(dirname "${BASH_SOURCE[0]}")/..
+
+missing_labels=$(find base -name "*.yaml" -print0 | xargs -0L1 -I {} sh -c "cat {} | yaml2json | jq --exit-status -f .buildkite/rbac.jq > /dev/null || echo {}")
+
+if [ ! -z "${missing_labels}" ]; then
+    echo "> Please add 'metadata.labels.category: rbac' to the following files:"
+    echo "${missing_labels}"
+    exit 1 
+fi
