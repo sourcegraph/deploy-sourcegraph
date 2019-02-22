d6da124f0e4235fab0fb4e878543dbd8e3694b17
ggilmore <geoffrey@sourcegraph.com>
2019-02-22 12:46:13 -0800

    add yaml2json installation script

---
+++ .buildkite/install-yaml2json.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION=1.3
+
+if [ ! $(which yaml2json) ]; then
+    echo "Installing yaml2json to /usr/local/bin"
+    wget https://github.com/bronze1man/yaml2json/releases/download/v${VERSION}/yaml2json_linux_amd64 -O /usr/local/bin/yaml2json
+    chmod a+x /usr/local/bin/yaml2json
+fi
