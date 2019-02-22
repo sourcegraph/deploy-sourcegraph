c753bea6ccd01faf1a9436a4c0bb80d9a280346b
ggilmore <geoffrey@sourcegraph.com>
2019-02-22 13:59:21 -0800

    both base and configure

--- .buildkite/verify-rbac-labels.sh
+++ .buildkite/verify-rbac-labels.sh
@@ -7,7 +7,7 @@ cd $(dirname "${BASH_SOURCE[0]}")/..
 
 .buildkite/install-yaml2json.sh
 
-missing_labels=$(find base -name "*.yaml" -print0 | xargs -0L1 -I {} sh -c "cat {} | yaml2json | jq --exit-status -f .buildkite/rbac.jq > /dev/null || echo {}")
+missing_labels=$(find base configure "*.yaml" -print0 | xargs -0L1 -I {} sh -c "cat {} | yaml2json | jq --exit-status -f .buildkite/rbac.jq > /dev/null || echo {}")
 
 if [ ! -z "${missing_labels}" ]; then
     echo "> Please add 'metadata.labels.category: rbac' to the following files:"
