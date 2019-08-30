a020791472fbb08a2b6968a3119296acdc7aee82
ggilmore <geoffrey@sourcegraph.com>
2019-08-30 16:31:41 -0700

    add build creator env to test script

--- .buildkite/integration-test.sh
+++ .buildkite/integration-test.sh
@@ -8,5 +8,6 @@ export DEPLOY_SOURCEGRAPH_ROOT=$(pwd)
 export TEST_GCP_PROJECT=sourcegraph-dev
 export TEST_GCP_ZONE=us-central1-a
 export TEST_GCP_USERNAME=buildkite@sourcegraph-dev.iam.gserviceaccount.com
+export BUILD_CREATOR=$BUILDKITE_BUILD_CREATOR
 
 go test ./... -v -timeout 25m
