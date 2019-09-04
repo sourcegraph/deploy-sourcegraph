2bbd3e53b8915b8d7240eb2146a6a7b99ee6d06d
ggilmore <geoffrey@sourcegraph.com>
2019-09-04 14:24:57 -0700

    swap to sourcegraph-server project

--- .buildkite/integration-test.sh
+++ .buildkite/integration-test.sh
@@ -5,7 +5,7 @@ set -ex
 cd $(dirname "${BASH_SOURCE[0]}")/..
 
 export DEPLOY_SOURCEGRAPH_ROOT=$(pwd)
-export TEST_GCP_PROJECT=sourcegraph-dev
+export TEST_GCP_PROJECT=sourcegraph-server
 export TEST_GCP_ZONE=us-central1-a
 export TEST_GCP_USERNAME=buildkite@sourcegraph-dev.iam.gserviceaccount.com
 export BUILD_CREATOR=$BUILDKITE_BUILD_CREATOR
