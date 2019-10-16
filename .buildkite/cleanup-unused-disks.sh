5f49108d5eb97dec8695cdd2475049d8fae80251
ggilmore <geoffrey@sourcegraph.com>
2019-10-16 16:04:15 -0700

    add script to delete unusued GCP disks
    - We have a limited disk quota on the Sourcegraph Auxiliary project, so it's important that unusued disks are pruned promptly.
    
    - Nobody should be creating long-lived services in the Sourcegraph Auxiliary project, so it's safe to delete these disks. (You should create a snapshot if you want to retain data after you delete an instance)
    
    - This should ultimately run as a cronjob on our CI cluster, but that requires some additional configuration. Adding it as a simple script in this repo is faster, and is still useful since this repository's integration create many of these disks.

---
+++ .buildkite/cleanup-unused-disks.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Deletes unattached GCP disks from the "Sourcegraph Auxiliary" project
+
+set -euxo pipefail
+
+PROJECT=sourcegraph-server
+
+gcloud_command() {
+    gcloud --quiet --project="$PROJECT" "$@"
+}
+
+echo "---Deleting unattached GCP disks from the $PROJECT project"
+
+for disk in gcloud_command compute disks list --filter="-users:*" --format="value(selfLink)"
+do
+    echo "Deleting disk: $disk"
+    gcloud_command compute disks delete $disk
+done
+
+echo "---Done deleting unattached GCP disks from the $PROJECT project"
