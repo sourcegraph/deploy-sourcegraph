06a01d58c6d382b6f02d2371f509defb19f1ae0a
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-02-28 22:05:31 -0800

    kubectl version is not working

--- .buildkite/verify-yaml.sh
+++ .buildkite/verify-yaml.sh
@@ -5,6 +5,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 gcloud container clusters get-credentials dogfood --zone us-central1-a --project sourcegraph-dev
 
+kubectl version
+
 kubectl apply --dry-run --validate -k base --context=gke_sourcegraph-dev_us-central1-a_dogfood
 kubectl apply --dry-run --validate --recursive -f configure/ --context=gke_sourcegraph-dev_us-central1-a_dogfood
 
