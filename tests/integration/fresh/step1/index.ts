3116aec34bf13c26e8592d83b1b235e172cad479
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-03-10 16:11:01 -0700

    upgrade pulumi version to 1.12.0 (#544)

--- tests/integration/fresh/step1/index.ts
+++ tests/integration/fresh/step1/index.ts
@@ -1,11 +1,11 @@
 import * as os from 'os'
 import * as path from 'path'
 
+import * as fg from 'fast-glob'
 import * as k8s from '@pulumi/kubernetes'
 
 import { k8sProvider } from './cluster'
 import { deploySourcegraphRoot, gcpUsername } from './config'
-import * as fs from "fs";
 
 const clusterAdmin = new k8s.rbac.v1.ClusterRoleBinding(
     'cluster-admin-role-binding',
@@ -48,43 +48,48 @@ const storageClass = new k8s.storage.v1.StorageClass(
     { provider: k8sProvider }
 )
 
-const getAllButKustomizeYAMLFiles = dir =>
-    fs.readdirSync(dir).reduce((files, file) => {
-        if (file == 'kustomization.yaml') {
-            return files;
-        }
-        const ext = file.substr(file.lastIndexOf('.') + 1);
-        const name = path.join(dir, file);
-        const isDirectory = fs.statSync(name).isDirectory();
-        if (!isDirectory && ext != 'yaml') {
-            return files;
-        }
-        return isDirectory ? [...files, ...getAllButKustomizeYAMLFiles(name)] : [...files, name];
-    }, []);
+const globOptions = {
+    ignore: ['**/kustomization.yaml'],
+}
 
-const baseDeployment = new k8s.yaml.ConfigGroup(
-    'base',
-    {
-        files: getAllButKustomizeYAMLFiles(path.posix.join(deploySourcegraphRoot, 'base')),
-    },
-    {
-        providers: { kubernetes: k8sProvider },
-        dependsOn: [clusterAdmin, storageClass],
-    }
+const baseFiles = fg(`${path.posix.join(deploySourcegraphRoot, 'base')}/**/*.yaml`, globOptions)
+
+const baseDeployment = baseFiles.then(
+    files =>
+        new k8s.yaml.ConfigGroup(
+            'base',
+            {
+                files,
+            },
+            {
+                providers: { kubernetes: k8sProvider },
+                dependsOn: [clusterAdmin, storageClass],
+            }
+        )
 )
 
-const ingressNginx = new k8s.yaml.ConfigGroup(
-    'ingress-nginx',
-    {
-        files: `${path.posix.join(deploySourcegraphRoot, 'configure', 'ingress-nginx')}/**/*.yaml`,
-    },
-    { providers: { kubernetes: k8sProvider }, dependsOn: clusterAdmin }
+const ingressNginxFiles = fg(
+    `${path.posix.join(deploySourcegraphRoot, 'configure', 'ingress-nginx')}/**/*.yaml`,
+    globOptions
 )
 
-export const ingressIP = ingressNginx
-    .getResource('v1/Service', 'ingress-nginx', 'ingress-nginx')
-    .apply(svc => svc.status)
-    .apply(status => status.loadBalancer.ingress.map(i => i.ip))
-    .apply(ips => (ips.length === 1 ? ips[0] : undefined))
+const ingressNginx = ingressNginxFiles.then(
+    files =>
+        new k8s.yaml.ConfigGroup(
+            'ingress-nginx',
+            {
+                files,
+            },
+            { providers: { kubernetes: k8sProvider }, dependsOn: clusterAdmin }
+        )
+)
+
+export const ingressIP = ingressNginx.then(ingress =>
+    ingress
+        .getResource('v1/Service', 'ingress-nginx', 'ingress-nginx')
+        .apply(svc => svc.status)
+        .apply(status => status.loadBalancer.ingress.map(i => i.ip))
+        .apply(ips => (ips.length === 1 ? ips[0] : undefined))
+)
 
 export * from './cluster'
