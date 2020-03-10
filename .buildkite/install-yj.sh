08eebfca7afa0017a053419477b104e6f7ceb60f
Geoffrey Gilmore <ggilmore@geoffreys-mbp.lan>
2020-03-10 14:29:10 -0700

    add debugging

--- .buildkite/install-yj.sh
+++ .buildkite/install-yj.sh
@@ -2,6 +2,9 @@
 
 VERSION=1.0.0
 
+which pulumi 
+ls /root/.asdf/shims
+
 if [ ! $(which yj) ]; then
     echo "Installing yj to /usr/local/bin"
     wget https://github.com/sourcegraph/yj/releases/download/v${VERSION}/yj-${VERSION}-linux-amd64 -O /usr/local/bin/yj
