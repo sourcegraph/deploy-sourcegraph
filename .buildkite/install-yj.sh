820911e90883b6dc4ca772d173e2f02f7bf213ff
Geoffrey Gilmore <ggilmore@geoffreys-mbp.lan>
2020-03-10 15:25:28 -0700

    install tool-versions from all subfolders

--- .buildkite/install-yj.sh
+++ .buildkite/install-yj.sh
@@ -2,9 +2,6 @@
 
 VERSION=1.0.0
 
-which pulumi 
-ls /root/.asdf/shims
-
 if [ ! $(which yj) ]; then
     echo "Installing yj to /usr/local/bin"
     wget https://github.com/sourcegraph/yj/releases/download/v${VERSION}/yj-${VERSION}-linux-amd64 -O /usr/local/bin/yj
