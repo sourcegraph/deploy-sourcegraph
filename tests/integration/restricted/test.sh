15422968cfe5ffc73cc5b3eaaad5e5eb2358c27b
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-03-31 13:28:51 -0700

    integration test label

--- tests/integration/restricted/test.sh
+++ tests/integration/restricted/test.sh
@@ -10,7 +10,7 @@ cd $(dirname "${BASH_SOURCE[0]}")
 
 # set up the cluster, set up the fake user and restricted policy and then deploy the non-privileged overlay as that user
 
-gcloud container clusters create ${CLUSTER_NAME} --zone ${TEST_GCP_ZONE} --num-nodes 3 --machine-type n1-standard-16 --disk-type pd-ssd --project ${TEST_GCP_PROJECT} --labels=cost-category=build,build-creator=${BUILD_CREATOR},build-branch=${BUILD_BRANCH}
+gcloud container clusters create ${CLUSTER_NAME} --zone ${TEST_GCP_ZONE} --num-nodes 3 --machine-type n1-standard-16 --disk-type pd-ssd --project ${TEST_GCP_PROJECT} --labels=cost-category=build,build-creator=${BUILD_CREATOR},build-branch=${BUILD_BRANCH},integration-test=fresh
 
 gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${TEST_GCP_ZONE} --project ${TEST_GCP_PROJECT}
 
