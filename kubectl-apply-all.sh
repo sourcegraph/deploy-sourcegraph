40c5150831964cfed38516da0a3c5f0a7316eba1
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-04-02 11:09:22 -0700

    revert prune testing

--- kubectl-apply-all.sh
+++ kubectl-apply-all.sh
@@ -7,5 +7,5 @@
 #   * Whenever the configuration for any resource has been updated
 
 # Apply the base Soucegraph deployment
-kubectl apply -l deploy=sourcegraph -k base/rbac-roles --prune
-kubectl apply -l deploy=sourcegraph -k base --prune
+kubectl apply -k base -l deploy=sourcegraph
+kubectl apply -k base/rbac-roles -l deploy=sourcegraph
