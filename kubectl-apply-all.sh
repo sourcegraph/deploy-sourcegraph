a0d48254bede5f12c1ebd88c85c6dc39633619af
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-04-03 13:33:28 -0700

    avoid kustomize in apply -f by listing all other files

--- kubectl-apply-all.sh
+++ kubectl-apply-all.sh
@@ -6,5 +6,8 @@
 #   * When the cluster is first created
 #   * Whenever the configuration for any resource has been updated
 
-# Apply the base Soucegraph deployment 
-kubectl apply --prune -l deploy=sourcegraph -f base --recursive
+# Apply the base Soucegraph deployment
+
+FILES=`find base -name "*.yaml" \( ! -name kustomization.yaml \)  | tr "\n" "," | sed 's/,$/ /' | tr " " "\n"`
+
+kubectl apply --prune -l deploy=sourcegraph -f ${FILES} --recursive
