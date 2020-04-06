17c238a86e73e6d6883864b522bf8a7980f5bc88
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-04-06 12:49:09 -0700

    more escaping needed

--- .buildkite/integration-restricted-test.sh
+++ .buildkite/integration-restricted-test.sh
@@ -8,7 +8,7 @@ export DEPLOY_SOURCEGRAPH_ROOT=$(pwd)
 export TEST_GCP_PROJECT=sourcegraph-server
 export TEST_GCP_ZONE=us-central1-a
 export TEST_GCP_USERNAME=buildkite@sourcegraph-dev.iam.gserviceaccount.com
-export BUILD_CREATOR=${BUILDKITE_BUILD_CREATOR// /-}
+export BUILD_CREATOR=${BUILDKITE_BUILD_CREATOR_EMAIL//[@.]/-}
 export BUILD_UUID=$BUILDKITE_BUILD_ID
 export BUILD_BRANCH=${BUILDKITE_BRANCH//./-}
 
