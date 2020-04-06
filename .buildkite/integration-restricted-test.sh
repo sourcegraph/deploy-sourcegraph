d3e8f3ecf1ce3885db1925177a64a17200bf8643
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-04-06 09:08:08 -0700

    escape branch for label use

--- .buildkite/integration-restricted-test.sh
+++ .buildkite/integration-restricted-test.sh
@@ -10,7 +10,7 @@ export TEST_GCP_ZONE=us-central1-a
 export TEST_GCP_USERNAME=buildkite@sourcegraph-dev.iam.gserviceaccount.com
 export BUILD_CREATOR=$BUILDKITE_BUILD_CREATOR
 export BUILD_UUID=$BUILDKITE_BUILD_ID
-export BUILD_BRANCH=$BUILDKITE_BRANCH
+export BUILD_BRANCH=${BUILDKITE_BRANCH//./-}
 
 ${DEPLOY_SOURCEGRAPH_ROOT}/tests/integration/restricted/test.sh
 
