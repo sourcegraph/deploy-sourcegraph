cbfbaece74f84953c98c3f6ae8ae61413f092c04
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-04-08 14:46:59 -0700

    shellcheck integration-test.sh

--- .buildkite/integration-test.sh
+++ .buildkite/integration-test.sh
@@ -2,25 +2,35 @@
 
 set -ex
 
-cd $(dirname "${BASH_SOURCE[0]}")/..
+cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
-export DEPLOY_SOURCEGRAPH_ROOT=$(pwd)
+DEPLOY_SOURCEGRAPH_ROOT=$(pwd)
+export DEPLOY_SOURCEGRAPH_ROOT
+
 export TEST_GCP_PROJECT=sourcegraph-server
 export TEST_GCP_ZONE=us-central1-a
 export TEST_GCP_USERNAME=buildkite@sourcegraph-dev.iam.gserviceaccount.com
-export BUILD_CREATOR="$(echo $BUILDKITE_BUILD_CREATOR | tr ' /@.' '_' | tr 'A-Z' 'a-z')"
 
-export GENERATED_BASE=`mktemp -d`
+BUILD_CREATOR="$(echo "${BUILDKITE_BUILD_CREATOR}" | tr ' /@.' '_' | tr '[:upper:]' '[:lower:]')"
+export BUILD_CREATOR
 
-kustomize build overlays/non-root-create-cluster -o ${GENERATED_BASE}
+GENERATED_BASE=$(mktemp -d)
+export GENERATED_BASE
 
-TEST_ARGS=( "test" "-timeout" "25m")
+cleanup() {
+    rm -rf "${GENERATED_BASE}"
+}
+trap cleanup EXIT
+
+kustomize build overlays/non-root-create-cluster -o "${GENERATED_BASE}"
+
+TEST_ARGS=("test" "-timeout" "25m")
 
 if [[ "${VERBOSE:-"false"}" == "true" ]]; then
-    TEST_ARGS+=( "-v" )
+    TEST_ARGS+=("-v")
 fi
 
-TEST_ARGS+=( "./..." )
+TEST_ARGS+=("./...")
 
 echo "--- Running integration tests"
 
