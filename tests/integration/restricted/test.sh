4153562e7c59d0929729a872f55c8f2fb5c4062e
Beyang Liu <beyang@sourcegraph.com>
2020-04-07 23:10:18 -0700

    add timeouts to integration test

--- tests/integration/restricted/test.sh
+++ tests/integration/restricted/test.sh
@@ -55,13 +55,13 @@ kubectl --as=system:serviceaccount:ns-sourcegraph:fake-user -n ns-sourcegraph ap
 
 # wait for it all to finish (we list out the ones with persistent volume claim because they take longer)
 
-kubectl -n ns-sourcegraph rollout status -w statefulset/indexed-search
-kubectl -n ns-sourcegraph rollout status -w deployment/precise-code-intel-bundle-manager
-kubectl -n ns-sourcegraph rollout status -w deployment/prometheus
-kubectl -n ns-sourcegraph rollout status -w deployment/redis-cache
-kubectl -n ns-sourcegraph rollout status -w deployment/redis-store
-kubectl -n ns-sourcegraph rollout status -w statefulset/gitserver
-kubectl -n ns-sourcegraph rollout status -w deployment/sourcegraph-frontend
+timeout 5m kubectl -n ns-sourcegraph rollout status -w statefulset/indexed-search
+timeout 5m kubectl -n ns-sourcegraph rollout status -w deployment/precise-code-intel-bundle-manager
+timeout 5m kubectl -n ns-sourcegraph rollout status -w deployment/prometheus
+timeout 5m kubectl -n ns-sourcegraph rollout status -w deployment/redis-cache
+timeout 5m kubectl -n ns-sourcegraph rollout status -w deployment/redis-store
+timeout 5m kubectl -n ns-sourcegraph rollout status -w statefulset/gitserver
+timeout 5m kubectl -n ns-sourcegraph rollout status -w deployment/sourcegraph-frontend
 
 # hit it with one request
 
