d59b58d655876662f4ba74bf47ada41fe8bebc31
Beyang Liu <beyang@sourcegraph.com>
2020-04-15 21:13:21 -0700

    rm dockerfiles (moved to sourcegraph/sourcegraph)

--- dockerfiles/jaeger/all-in-one/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-. version.sh
-
-echo "Building image for Jaeger $VERSION"
-
-docker build --build-arg JAEGER_VERSION="$VERSION" . -t "sourcegraph/jaeger-all-in-one:$VERSION"
--- dockerfiles/jaeger/all-in-one/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-. version.sh
-
-echo "Uploading image for Jaeger $VERSION"
-
-docker push "sourcegraph/jaeger-all-in-one:$VERSION"
