fbc470cefb790724d4fbee0cd195fe68b0ad234e
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-04-27 13:42:48 -0700

    fixes after testing

--- overlay-generate-cluster.sh
+++ overlay-generate-cluster.sh
@@ -15,4 +15,6 @@ cp -R overlays "${BUILD_DIR}"
 cp -R base "${BUILD_DIR}"/overlays/bases/deployments/base
 cp -R base "${BUILD_DIR}"/overlays/bases/rbac-roles/base
 
+mkdir -p $2
+
 kustomize build "${BUILD_DIR}"/overlays/$1 -o $2
