4f26f621a813f9d1d3ef868396532a22dcc06d41
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-05-14 12:11:08 -0700

    sh

--- .buildkite/install-src.sh
+++ .buildkite/install-src.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 if [ ! "$(which src-alpha)" ]; then
-    echo "Installing pre-release src to /usr/local/bin"
-    mkdir -p /tmp/src-alpha
-    cd /tmp/src-alpha
-    wget https://github.com/sourcegraph/src-cli/releases/download/3.13.0-alpha/src-cli_3.13.0-SNAPSHOT-9f58a28_linux_amd64.tar.gz
-    tar xvzf src-cli_3.13.0-SNAPSHOT-9f58a28_linux_amd64.tar.gz
-    cp src /usr/local/bin/src-alpha
-    chmod a+x /usr/local/bin/src-alpha
+  echo "Installing pre-release src to /usr/local/bin"
+  mkdir -p /tmp/src-alpha
+  cd /tmp/src-alpha
+  wget https://github.com/sourcegraph/src-cli/releases/download/3.13.0-alpha/src-cli_3.13.0-SNAPSHOT-9f58a28_linux_amd64.tar.gz
+  tar xvzf src-cli_3.13.0-SNAPSHOT-9f58a28_linux_amd64.tar.gz
+  cp src /usr/local/bin/src-alpha
+  chmod a+x /usr/local/bin/src-alpha
 fi
-
