1a4c43d82648bae557991278e5434319ce89027c
Geoffrey Gilmore <tkdmaster123@gmail.com>
2020-05-14 13:54:36 -0700

    update insiders tags

--- generated/params.sh
+++ generated/params.sh
@@ -10,7 +10,7 @@ export TRANSFORMATIONS=(
   # 'set_replicas searcher 10'
   'set_replicas searcher 5'
 
-  'set_container_image frontend frontend index.docker.io/sourcegraph/frontend:insiders@sha256:6a69d89c7973bb286536fa0ff22624818ca51a2f0427dd32809190b940fe557a'
+  'set_container_image frontend frontend index.docker.io/sourcegraph/frontend:insiders@sha256:81aeefd5dd8b333ee224180767aad142e8308d38ed1fb2d834aefbdcd4652d1d'
   'set_cpu_limit frontend frontend 8000m'
   'set_memory_limit frontend frontend 6G'
   'set_cpu_request frontend frontend 6000m'
@@ -22,17 +22,17 @@ export TRANSFORMATIONS=(
   'set_annotation frontend Ingress nginx.ingress.kubernetes.io/proxy-read-timeout 1d'
   'set_frontend_ingress_ssl k8s.sgdev.org sourcegraph-tls'
 
-  'set_container_image github-proxy github-proxy index.docker.io/sourcegraph/github-proxy:insiders@sha256:3a9e323988309c78c64745b0599e1b4c2b10ddcf2abbdb2283dbde038a4528ba'
+  'set_container_image github-proxy github-proxy index.docker.io/sourcegraph/github-proxy:insiders@sha256:d3498406db3493eb252c79948915eac1e1c7223f01f3500bc8b839676042cf88'
 
   'set_gitserver_replicas 4'
-  'set_container_image gitserver gitserver index.docker.io/sourcegraph/gitserver:insiders@sha256:e46e61c702345fb55206b87973f7c6b7f07fac0735201080b5b1e119605ce75c'
+  'set_container_image gitserver gitserver index.docker.io/sourcegraph/gitserver:insiders@sha256:55b5c1c872811e289a0e69abbac3b83a9ce627e7c08f92165f4bdc979d570310'
   'set_stateful_set_persistent_volume_claim_capacity gitserver repos 1Ti'
   'set_gitserver_ssh_credentials gitserver-ssh'
 
   'set_cpu_limit grafana grafana 100m'
   'set_memory_limit grafana grafana 100Mi'
   'set_memory_request grafana grafana 100Mi'
-  'set_container_image grafana grafana index.docker.io/sourcegraph/grafana:insiders@sha256:da9678fd7f79afb0b01b2a3f116a208d19c795f77f418186ba3a178055caa94f'
+  'set_container_image grafana grafana index.docker.io/sourcegraph/grafana:insiders@sha256:f3fbf9845f0b65377f90a7a8eac253b90b77f9e53242e559031ccde247836776'
   'add_environment_variable grafana grafana GF_SERVER_ROOT_URL <THE_VALUE>'
 
   'set_cpu_limit pgsql pgsql 16'
@@ -52,24 +52,24 @@ export TRANSFORMATIONS=(
 
   'set_memory_limit prometheus prometheus 2G'
   'set_memory_request prometheus prometheus 500M'
-  'set_container_image prometheus prometheus index.docker.io/sourcegraph/prometheus:insiders@sha256:473a5c94769bf9f73451f6727942202c920d6524f113b9f2cae7edaa033ca552'
+  'set_container_image prometheus prometheus index.docker.io/sourcegraph/prometheus:insiders@sha256:d0d80a7598b907624f106deebe5ecd7ba5c16ed47d913abee2085e18331f9a90'
 
-  'set_container_image precise-code-intel/api-server precise-code-intel-api-server index.docker.io/sourcegraph/precise-code-intel-api-server:insiders@sha256:aab8c6878c2625ac8a1a337707dd4abdfc321896983fc645ac21585276f60599'
-  'set_container_image precise-code-intel/bundle-manager precise-code-intel-bundle-manager index.docker.io/sourcegraph/precise-code-intel-bundle-manager:insiders@sha256:12014e40198bb7b851a79965a67bc577ac4b491a77254ccf32ee3b1836421ce7'
-  'set_container_image precise-code-intel/worker precise-code-intel-worker index.docker.io/sourcegraph/precise-code-intel-worker:insiders@sha256:bc93f677e18f8f1cc63725e660e877b601465962676abd43f4fb8947783ce135'
+  'set_container_image precise-code-intel/api-server precise-code-intel-api-server index.docker.io/sourcegraph/precise-code-intel-api-server:insiders@sha256:654499ae605b6a1480a7f8aab84dfc621e77214ba849cde12bce10a34bbece5d'
+  'set_container_image precise-code-intel/bundle-manager precise-code-intel-bundle-manager index.docker.io/sourcegraph/precise-code-intel-bundle-manager:insiders@sha256:7c38143e1648b3679766984ea75f8dcac4577e3a71768b814860cf12de9f44ff'
+  'set_container_image precise-code-intel/worker precise-code-intel-worker index.docker.io/sourcegraph/precise-code-intel-worker:insiders@sha256:9acc4feb17fab17a1c34afa0ac42f930af438bf6ca264a91aa00dccf5eac4ef1'
 
-  'set_container_image query-runner query-runner index.docker.io/sourcegraph/query-runner:insiders@sha256:55d85a1315b2ec52c38a70671bc908a114cd5ad89fd2eadc95eb7208c69c952a'
+  'set_container_image query-runner query-runner index.docker.io/sourcegraph/query-runner:insiders@sha256:c1a64022fb07c7ad440f91505daf3833f773198a4313831143b0a8bc580fc848'
 
   'set_container_image redis/redis-cache redis-cache index.docker.io/sourcegraph/redis-cache:insiders@sha256:7820219195ab3e8fdae5875cd690fed1b2a01fd1063bd94210c0e9d529c38e56'
   'set_container_image redis/redis-store redis-store index.docker.io/sourcegraph/redis-store:insiders@sha256:e8467a8279832207559bdfbc4a89b68916ecd5b44ab5cf7620c995461c005168'
 
-  'set_container_image replacer replacer index.docker.io/sourcegraph/replacer:insiders@sha256:624b11db92d0a7fd281c28aa4c3723818f7145413dbedd1ef280a77d19123db5'
+  'set_container_image replacer replacer index.docker.io/sourcegraph/replacer:insiders@sha256:20237ba8463f7b4523e2d651412d0ac4f512c21bb1a5e6553caec2caac6ea54a'
 
-  'set_container_image repo-updater repo-updater index.docker.io/sourcegraph/repo-updater:insiders@sha256:868f2ee00d3fdc58b164b2f802e1f7ba8404c8e88364b0cf4c89a1339c3702d3'
+  'set_container_image repo-updater repo-updater index.docker.io/sourcegraph/repo-updater:insiders@sha256:5b2d5f6b62613d7da40c5fa16366fc3aba660426ec102692b7e30a2a3b708b36'
 
-  'set_container_image searcher searcher index.docker.io/sourcegraph/searcher:insiders@sha256:df4a648676bc7e5d49cac3b1de0b6b8550c2fd9a6a0b3953efe1ea40fe371b0e'
+  'set_container_image searcher searcher index.docker.io/sourcegraph/searcher:insiders@sha256:db7b6ed8be158e7f39a72be27310d3ae745e8be99215db52f7ac0a4bc6e830e7'
 
-  'set_container_image symbols symbols index.docker.io/sourcegraph/symbols:insiders@sha256:59f8e39244d9a777b5d8f36a8686b470499bc465b35a5f2f07f3979c43da1a80'
+  'set_container_image symbols symbols index.docker.io/sourcegraph/symbols:insiders@sha256:ea4fcf06a0639e9430eb86c32ce1eed790a581b8c60689cff7618b7a8f4b36ba'
 
   'set_container_image syntect-server syntect-server index.docker.io/sourcegraph/syntax-highlighter:insiders@sha256:aa93514b7bc3aaf7a4e9c92e5ff52ee5052db6fb101255a69f054e5b8cdb46ff'
 
