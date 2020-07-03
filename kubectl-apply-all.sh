37b460ac3e31a20b138e5849c0f4be35f8b88bac
davejrt <2067825+davejrt@users.noreply.github.com>
2020-07-03 12:06:55 -0500

    update advice for --prune flag (#783)
    * update advice for --prune flag
    
    Co-authored-by: uwedeportivo <534011+uwedeportivo@users.noreply.github.com>

--- kubectl-apply-all.sh
+++ kubectl-apply-all.sh
@@ -5,6 +5,10 @@
 # This file should be run:
 #   * When the cluster is first created
 #   * Whenever the configuration for any resource has been updated
-
+#
+# The --prune flag is destructive and should always be used 
+# in conjunction with -f base and -l deploy=sourcegraph. Otherwise, it will delete all resources 
+# previously created by create or apply that are not specified in the command.
+#
 # Apply the base Soucegraph deployment
 kubectl apply --prune -l deploy=sourcegraph -f base --recursive $@
