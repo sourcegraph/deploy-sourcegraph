18f17cee63afd4f92c1941ec491ef76183647a88
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-07-03 12:35:12 -0700

    fix src release filename

--- tests/integration/restricted/install-src.sh
+++ tests/integration/restricted/install-src.sh
@@ -5,7 +5,7 @@ if [ ! "$(which src-alpha)" ]; then
   mkdir -p /tmp/src-alpha
   cd /tmp/src-alpha
   wget https://github.com/sourcegraph/src-cli/releases/download/3.18.0-alpha/src-cli_3.18.0-alpha-SNAPSHOT-3b23f74_linux_amd64.tar.gz
-  tar xvzf src-cli_3.13.0-SNAPSHOT-9f58a28_linux_amd64.tar.gz
+  tar xvzf src-cli_3.18.0-alpha-SNAPSHOT-3b23f74_linux_amd64.tar.gz
   cp src /usr/local/bin/src-alpha
   chmod a+x /usr/local/bin/src-alpha
 fi
