708b1cf10e94f0e82d5117598cfb4f8d21b7cf9f
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-07-03 12:18:03 -0700

    validate command as json spec

--- tests/integration/restricted/install-src.sh
+++ tests/integration/restricted/install-src.sh
@@ -4,7 +4,7 @@ if [ ! "$(which src-alpha)" ]; then
   echo "Installing pre-release src to /usr/local/bin"
   mkdir -p /tmp/src-alpha
   cd /tmp/src-alpha
-  wget https://github.com/sourcegraph/src-cli/releases/download/3.13.0-alpha/src-cli_3.13.0-SNAPSHOT-9f58a28_linux_amd64.tar.gz
+  wget https://github.com/sourcegraph/src-cli/releases/download/3.18.0-alpha/src-cli_3.18.0-alpha-SNAPSHOT-3b23f74_linux_amd64.tar.gz
   tar xvzf src-cli_3.13.0-SNAPSHOT-9f58a28_linux_amd64.tar.gz
   cp src /usr/local/bin/src-alpha
   chmod a+x /usr/local/bin/src-alpha
