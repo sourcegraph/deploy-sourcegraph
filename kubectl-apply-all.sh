fd8ae81269ab6a14507e5740c6622adbb0e9206d
Robert Lin <robert@bobheadxi.dev>
2020-07-13 17:06:43 +0800

    prometheus: add consistent alert labels (#786)

--- kubectl-apply-all.sh
+++ kubectl-apply-all.sh
@@ -6,9 +6,10 @@
 #   * When the cluster is first created
 #   * Whenever the configuration for any resource has been updated
 #
-# The --prune flag is destructive and should always be used 
-# in conjunction with -f base and -l deploy=sourcegraph. Otherwise, it will delete all resources 
+# The --prune flag is destructive and should always be used
+# in conjunction with -f base and -l deploy=sourcegraph. Otherwise, it will delete all resources
 # previously created by create or apply that are not specified in the command.
 #
 # Apply the base Soucegraph deployment
+# shellcheck disable=SC2068
 kubectl apply --prune -l deploy=sourcegraph -f base --recursive $@
