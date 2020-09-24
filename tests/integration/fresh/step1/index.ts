eeeaed9200843db349beb598bea34bea2c522007
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2020-09-24 19:03:40 +0200

    Prettier

--- tests/integration/fresh/step1/index.ts
+++ tests/integration/fresh/step1/index.ts
@@ -58,7 +58,7 @@ const ingressNginx = ingressNginxFiles.then(
             {
                 files,
             },
-            { providers: { kubernetes: k8sProvider }}
+            { providers: { kubernetes: k8sProvider } }
         )
 )
 
