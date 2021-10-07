d95b32aab1442c93da5841543751b7f16f1eb6fb
Dave Try <davetry@gmail.com>
2021-10-07 13:47:19 -0500

    makes regex more precise, add docstring, exit on first failure

--- .buildkite/check-image-names/check-image-names.go
+++ .buildkite/check-image-names/check-image-names.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 
 	"github.com/pkg/errors"
-	images "github.com/sourcegraph/sourcegraph/enterprise/dev/ci/images"
+	"github.com/sourcegraph/sourcegraph/enterprise/dev/ci/images"
 )
 
 func main() {
@@ -23,7 +23,7 @@ func main() {
 
 func getImages(dir string) error {
 
-	i := regexp.MustCompile("sourcegraph/[a-z0-9-_.]+:")
+	i := regexp.MustCompile(`image: index\.docker\.io\/sourcegraph/(?P<image>[a-z0-9-_.]+):[a-z0-9-_]+@sha256:[[:alnum:]]+`)
 	var matches []string
 
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
@@ -43,21 +43,24 @@ func getImages(dir string) error {
 			return errors.Wrap(err, "when reading file contents")
 		}
 
+		// matchedImages contains all lines matching our regex. FindAllSubmatch returns
+		// a slice containing the full string, and the capture group `image` for each image in a file.
+		// We then loop over each slice, pull out the capture group and append it to a list of images
+		// to compare with upstream.
 		matchedImages := i.FindAllSubmatch(data, -1)
 		for _, match := range matchedImages {
-			match := strings.Replace(string(match[0]), "sourcegraph/", "", -1)
-			match = strings.Replace(match, ":", "", -1)
-			matches = append(matches, match)
+			matchd := string(match[1])
+			matches = append(matches, matchd)
 		}
 
 		return nil
 
 	})
-
 	matches = Unique(matches)
 	for i, image := range matches {
-		if image != images.SourcegraphDockerImages[i] {
-			fmt.Printf("image: %s was found in is not in the upstream list.\n", image)
+		if image != images.DeploySourcegraphDockerImages[i] {
+			fmt.Printf("image: %s is not in the upstream list.\n", image)
+			os.Exit(1)
 		}
 	}
 
