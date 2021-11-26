69999ca9461da4a6eeae573c4ce1a0388806c327
Dave Try <davetry@gmail.com>
2021-11-26 18:19:37 -0500

    Revert "update upstream controller"
    This reverts commit 7c3a01ca6ca9a52816b449f8bd2d707c34258b6f.

--- configure/ingress-nginx/install.sh
+++ configure/ingress-nginx/install.sh
@@ -4,4 +4,5 @@ set -ex
 
 cd "$(dirname ${BASH_SOURCE[0]})"
 
-kubectl apply -f ingress-nginx.yaml
+kubectl apply -f mandatory.yaml # this should be deployed first so that the namespace is created
+kubectl apply -f cloud-generic.yaml
