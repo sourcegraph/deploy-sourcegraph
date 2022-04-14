656e916d57484afc642d363f97216b26594e6d22
Dave Try <davetry@gmail.com>
2022-04-14 19:52:03 +0000

    rename file

---
+++ .buildkite/verify-overlays.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux
+pushd "$(dirname "${BASH_SOURCE[0]}")"/..
+
+OVERLAYS=()
+mapfile -t OVERLAYS < <(find overlays -maxdepth 1 -type d ! -name '.*' ! -name 'bases' ! -name 'overlays' -printf '%f\n')
+
+for o in "${OVERLAYS[@]}"; do
+  DIR=generated-cluster-${o}
+  mkdir "${DIR}"
+  ./overlay-generate-cluster.sh "${o}" "${DIR}"
+  rm -rf "${DIR}"
+done
