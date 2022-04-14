8ef1bf05487633c8c1a0141f8f171aacded4a636
Dave Try <davetry@gmail.com>
2022-04-14 19:58:56 +0000

    fix logging

--- .buildkite/verify-overlays.sh
+++ .buildkite/verify-overlays.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-set -eux
+set -eu
 pushd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 OVERLAYS=()
 mapfile -t OVERLAYS < <(find overlays -maxdepth 1 -type d ! -name '.*' ! -name 'bases' ! -name 'overlays' -printf '%f\n')
 
-for o in "${OVERLAYS[@]}"; do
-  echo "<<<< GENERATING ${o} OVERLAYS >>>>>"
-  DIR=generated-cluster-${o}
+for O in "${OVERLAYS[@]}"; do
+  echo "<<<< GENERATING ${O} OVERLAY >>>>>"
+  DIR=generated-cluster-${O}
   mkdir "${DIR}"
-  ./overlay-generate-cluster.sh "${o}" "${DIR}"
+  ./overlay-generate-cluster.sh "${O}" "${DIR}"
   rm -rf "${DIR}"
 done
