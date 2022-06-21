682f7b75a6c1d6e628194e7037b1bc769b8023f5
Robert Lin <robert@bobheadxi.dev>
2022-06-21 11:13:36 -0700

    test/restricted: use a more descriptive namespace, do targeted disk prune for each test (#4144)
    Co-authored-by: Jean-Hadrien Chabran <jh@chabran.fr>

--- .buildkite/cleanup-disks.sh
+++ .buildkite/cleanup-disks.sh
@@ -11,3 +11,7 @@ export TEST_GCP_ZONE=us-central1-a
 # https://github.com/sourcegraph/sourcegraph/issues/32916 will implement long-term fix
 gcloud compute disks list --filter="name:gke-ds-test AND NOT users:*" --format="value(name)" --project ${TEST_GCP_PROJECT} |
   while read -r disk; do gcloud compute disks delete ${disk} --zone ${TEST_GCP_ZONE} --project ${TEST_GCP_PROJECT} --quiet; done
+
+# Also delete disks created by integration-restricted-test
+gcloud compute disks list --filter="description:ds-test-restricted AND NOT users:*" --format="value(name)" --project ${TEST_GCP_PROJECT} |
+  while read -r disk; do gcloud compute disks delete ${disk} --zone ${TEST_GCP_ZONE} --project ${TEST_GCP_PROJECT} --quiet; done
