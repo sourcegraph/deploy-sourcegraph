a954243e3e5929d86ec1d6d6a1aae35e8027e73a
Beyang Liu <beyang@sourcegraph.com>
2022-06-25 22:02:41 -0700

    customization program

---
+++ better-customize/index.ts
@@ -0,0 +1,224 @@
+import * as k8s from "@kubernetes/client-node";
+import { fstat, readdirSync, readFile, readFileSync } from "fs";
+import * as fs from "fs";
+import * as YAML from "yaml";
+import * as path from "path";
+import { PersistentVolume } from "@pulumi/kubernetes/core/v1";
+import * as mkdirp from "mkdirp";
+import { Cluster, Config, normalize, Transform } from "./common";
+import { normalizeOptions, normalizeYAMLRecursive } from './utils/normalize'
+
+(async function () {
+  if (process.env.NORMALIZE) {
+    await normalizeYAMLRecursive(process.env.NORMALIZE)
+    return
+  }
+
+  const configImportPath = process.env.EXAMPLE ? `./examples/${process.env.EXAMPLE}/customize` : './customize'
+  const config: Config = await (await import(configImportPath)).configuration()
+  const cluster: Cluster = {
+    Deployments: [],
+    PersistentVolumeClaims: [],
+    PersistentVolumes: [],
+    Services: [],
+    ClusterRoles: [],
+    ClusterRoleBindings: [],
+    ConfigMaps: [],
+    DaemonSets: [],
+    Ingresss: [],
+    PodSecurityPolicys: [],
+    Roles: [],
+    RoleBindings: [],
+    Secrets: [],
+    ServiceAccounts: [],
+    StatefulSets: [],
+    StorageClasses: [],
+    RawFiles: [],
+    Unrecognized: [],
+    ManualInstructions: [],
+  };
+
+function readCluster(root: string) {
+  const contents = readdirSync(root, { withFileTypes: true });
+  for (const entry of contents) {
+    if (entry.isFile()) {
+      if (entry.name.endsWith(".yaml")) {
+        const k8sType = path.extname(
+          entry.name.substring(0, entry.name.length - ".yaml".length)
+          );
+          const filename = path.join(root, entry.name);
+          switch (k8sType) {
+            case ".Deployment":
+            cluster.Deployments.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".PersistentVolumeClaim":
+            cluster.PersistentVolumeClaims.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".PersistentVolume":
+            cluster.PersistentVolumes.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".Service":
+            case ".IndexerService":
+            cluster.Services.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".ClusterRole":
+            cluster.ClusterRoles.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".ClusterRoleBinding":
+            cluster.ClusterRoleBindings.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".ConfigMap":
+            cluster.ConfigMaps.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".DaemonSet":
+            cluster.DaemonSets.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".Ingress":
+            cluster.Ingresss.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".PodSecurityPolicy":
+            cluster.PodSecurityPolicys.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".Role":
+            cluster.Roles.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".RoleBinding":
+            cluster.RoleBindings.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".ServiceAccount":
+            cluster.ServiceAccounts.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            case ".StatefulSet":
+            cluster.StatefulSets.push([
+              config.filenameMapper(config.sourceDirectory, filename),
+              YAML.parse(readFileSync(filename).toString()),
+            ]);
+            break;
+            default:
+            cluster.Unrecognized.push(entry.name);
+          }
+        }
+      } else if (entry.isDirectory()) {
+        readCluster(path.join(root, entry.name));
+      } else {
+        console.error("Ignoring unrecognized file type, name: ", entry.name);
+      }
+    }
+  }
+  readCluster(config.sourceDirectory);
+
+  try {
+    for (const t of config.transformations) {
+      await t(cluster);
+    }
+  } catch (error) {
+      console.error("Failed to generate manifest: ", error)
+  }
+
+  async function writeCluster(c: Cluster) {
+    const fileContents = [];
+    fileContents.push(
+      ...c.Deployments,
+      ...c.PersistentVolumeClaims,
+      ...c.PersistentVolumes,
+      ...c.Services,
+      ...c.ClusterRoles,
+      ...c.ClusterRoleBindings,
+      ...c.ConfigMaps,
+      ...c.DaemonSets,
+      ...c.Ingresss,
+      ...c.PodSecurityPolicys,
+      ...c.Roles,
+      ...c.RoleBindings,
+      ...c.ServiceAccounts,
+      ...c.StatefulSets,
+      ...c.StorageClasses,
+      ...c.Secrets
+    );
+
+    // TODO: do the rm -rf ouptputDirectory in TypeScript
+    
+    await Promise.all(
+      fileContents.map(async (c) => {
+        const filename = path.join(config.outputDirectory, c[0]);
+        const directory = path.dirname(filename);
+        await mkdirp(directory);
+        fs.writeFileSync(filename, YAML.stringify(c[1], normalizeOptions));
+      })
+    );
+    for (const [name, contents] of c.RawFiles) {
+      fs.writeFileSync(path.join(config.outputDirectory, name), contents);
+    }
+
+    // copy directory recursively
+    const copy = (source: string, target: string) => {
+      const files = readdirSync(source);
+      for (const file of files) {
+        const sourceFile = path.join(source, file);
+        const targetFile = path.join(target, file);
+        if (fs.lstatSync(sourceFile).isDirectory()) {
+          copy(sourceFile, targetFile);
+        } else {
+          // TODO: check if directory already exists
+          fs.copyFileSync(sourceFile, targetFile);
+        }
+      }
+    }
+    for (const manifestDir of config.additionalManifestDirectories || []) {
+      copy(manifestDir, config.outputDirectory);
+    }
+
+    if (c.ManualInstructions.length > 0) {
+      console.log(
+        "####################\n# Additional steps #\n####################\n"
+      );
+
+      for (const instruction of c.ManualInstructions) {
+        console.log(instruction + "\n\n\n");
+      }
+    }
+  }
+
+  await writeCluster(cluster);
+})().then(() => {
+  console.log("Done");
+});
