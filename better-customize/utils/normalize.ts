bc1f28c8d429826d0783172bf506b1e412fb3abe
Beyang Liu <beyang@sourcegraph.com>
2022-06-25 22:03:02 -0700

    normalize util

---
+++ better-customize/utils/normalize.ts
@@ -0,0 +1,29 @@
+import * as YAML from "yaml";
+import { resolve } from "dns";
+import { readdirSync, readFile, writeFile } from "fs";
+import * as path from "path";
+
+export const normalizeOptions = {
+    sortMapEntries: true,
+    lineWidth: 0,
+}
+
+export async function normalizeYAMLRecursive(root: string) {
+    const contents = readdirSync(root, { withFileTypes: true })
+    contents.map(async entry => {
+        if (entry.isFile()) {
+            const filename = path.join(root, entry.name)
+            if (entry.name.endsWith(".yaml")) {
+                const contents = await new Promise<Buffer>((resolve, reject) => readFile(filename, {}, (err, data) => err ? reject(err) : resolve(data)))
+                await new Promise<void>((resolve, reject) => writeFile(
+                    filename,
+                    YAML.stringify(YAML.parse(contents.toString()), { sortMapEntries: true, lineWidth: 0 }),
+                    {},
+                    err => err ? reject(err) : resolve()
+                ))
+            }
+        } else {
+            return normalizeYAMLRecursive(path.join(root, entry.name))
+        }
+    })
+}
