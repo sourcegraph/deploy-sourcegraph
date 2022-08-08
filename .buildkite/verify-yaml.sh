65c6e63a0a0cff6b128c1569f39fcadc180d8b74
leo <leo.p@sourcegraph.com>
2022-08-08 11:18:26 -0400

    add changes from "Fix target cluster #4158" (#4160)

--- .buildkite/verify-yaml.sh
+++ .buildkite/verify-yaml.sh
@@ -3,9 +3,9 @@
 set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-gcloud container clusters get-credentials dogfood --zone us-central1-f --project sourcegraph-dogfood
+gcloud container clusters get-credentials cloud --zone us-central1-f --project sourcegraph-dev
 
-kubectl apply --dry-run --validate --recursive -f base/ --context=gke_sourcegraph-dogfood_us-central1-f_dogfood
-kubectl apply --dry-run --validate --recursive -f configure/ --context=gke_sourcegraph-dogfood_us-central1-f_dogfood
+kubectl apply --dry-run --validate --recursive -f base/ --context=gke_sourcegraph-dev_us-central1-f_cloud
+kubectl apply --dry-run --validate --recursive -f configure/ --context=gke_sourcegraph-dev_us-central1-f_cloud
 
 .buildkite/verify-label.sh
