1daf2de169d29669f64f2491b0329aefc5d5d62e
Beatrix <beatrix@sourcegraph.com>
2022-11-04 10:33:19 -0700

    Update CI and scripts to use kustomize

--- .buildkite/verify-yaml.sh
+++ .buildkite/verify-yaml.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 gcloud container clusters get-credentials cloud --zone us-central1-f --project sourcegraph-dev
 
-kubectl apply --dry-run --validate --recursive -f base/ --context=gke_sourcegraph-dev_us-central1-f_cloud
-kubectl apply --dry-run --validate --recursive -f configure/ --context=gke_sourcegraph-dev_us-central1-f_cloud
+kubectl apply --dry-run --validate --recursive -k base/ --context=gke_sourcegraph-dev_us-central1-f_cloud
+kubectl apply --dry-run --validate --recursive -k configure/ --context=gke_sourcegraph-dev_us-central1-f_cloud
 
 .buildkite/verify-label.sh
