65ad3235c4beb79b486608f423c488a03ab405cc
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2022-11-11 10:37:33 +0200

    specify stdin for option -f

--- .buildkite/verify-yaml.sh
+++ .buildkite/verify-yaml.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 gcloud container clusters get-credentials cloud --zone us-central1-f --project sourcegraph-dev
 
-kustomize build ./base/. | kubectl apply --dry-run --validate -f --context=gke_sourcegraph-dev_us-central1-f_cloud -
-kustomize build ./configure/. | kubectl apply --prune -l deploy=sourcegraph -f --dry-run --validate --context=gke_sourcegraph-dev_us-central1-f_cloud -
+kustomize build ./base/. | kubectl apply --dry-run --validate --context=gke_sourcegraph-dev_us-central1-f_cloud -f -
+kustomize build ./configure/. | kubectl apply --prune -l deploy=sourcegraph --dry-run --validate --context=gke_sourcegraph-dev_us-central1-f_cloud -f -
 
 .buildkite/verify-label.sh
