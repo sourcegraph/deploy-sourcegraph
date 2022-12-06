c291c58efc6fb1a5c7007bc1d0489f99e8734ae0
Beyang Liu <beyang@sourcegraph.com>
2022-12-06 14:10:28 -0800

    .

--- better-customize/common.ts
+++ better-customize/common.ts
@@ -485,8 +485,6 @@ export const nonRoot = (): Transform => async (c: Cluster) => {
   c.StatefulSets.forEach(([, ss]) => update(ss));
   c.DaemonSets.forEach(([, ds]) => update(ds));
 
-  // TODO: supply separate role bindings, put these in a separate directory, `like non-privileged-roles`
-  // and add an `include` function here to include them
   c.ClusterRoleBindings = []
   c.ClusterRoles = []
   c.RoleBindings.push([
