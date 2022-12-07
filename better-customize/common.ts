390aba938cb4f828399f5d66eb8d87dc7ad22840
Beyang Liu <beyang@sourcegraph.com>
2022-12-07 01:28:51 -0800

    .

--- better-customize/common.ts
+++ better-customize/common.ts
@@ -549,7 +549,47 @@ export function defaultFilenameMapper(sourceDir: string, filename: string): stri
   return path.relative(sourceDir, filename)
 }
 
+export function kustomizeFilenameMapper(sourceDir: string, filename: string): string {
+  const rel = path.relative(sourceDir, filename);
 
+  const yaml = YAML.parse(readFileSync(filename).toString())
+  const dirParts = path.dirname(rel).split(path.sep)
+  const baseParts = path.basename(filename).split('.')
+  if (baseParts.length < 3) {
+    console.log('ERROR: could not transform filename', filename)
+    return filename
+  }
+  let [name, kind, ext] = baseParts
+  let prefix = 'apps_v1'
+
+  {
+    // Adjustments
+    if (dirParts.length > 0) {
+      const dirName = dirParts[dirParts.length-1]
+      if ([name, 'otel-collector', 'frontend', 'redis', 'jaeger', '.'].indexOf(dirName) === -1 && name.indexOf(`${dirName}-`) !== 0) {
+        name = dirName + '-' + name
+      }
+    }
+    const mappings: { [key: string]: string } = {
+      'codeinsights-db': 'codeinsights-db-conf', // TODO: only apply this on the configmap change...
+      'codeintel-db': 'codeintel-db-conf',
+      'pgsql': 'pgsql-conf',
+    }
+    if (kind.toLowerCase() === 'configmap' && mappings[name]) {
+      name = mappings[name]
+    }
+  }
+  
+  if (typeof yaml.apiVersion === 'string' || yaml.apiVersion instanceof String) {
+    prefix = (yaml.apiVersion as string).replace('/', '_')
+  }
+  
+  if (kind === 'IndexerService' && name === 'indexed-search') {
+    return 'v1_service_indexed-search-indexer.yaml'
+  }
+  
+  return prefix + '_' + kind.toLowerCase() + '_' + name.toLowerCase() + '.' + ext
+}
 
 const updateEnvironment = (
   curenv: Array<k8s.V1EnvVar>,
