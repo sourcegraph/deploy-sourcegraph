ad122391ecb25224c89a63effe505e20c8a72e67
Beatrix <beatrix@sourcegraph.com>
2022-12-14 11:55:26 -0800

    Fix verify script

--- .buildkite/verify-yaml.sh
+++ .buildkite/verify-yaml.sh
@@ -6,6 +6,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 gcloud container clusters get-credentials cloud --zone us-central1-f --project sourcegraph-dev
 
 kustomize build ./base/. | kubectl apply --dry-run --validate --context=gke_sourcegraph-dev_us-central1-f_cloud -f -
-kustomize build ./configure/. | kubectl apply --prune -l deploy=sourcegraph --dry-run --validate --context=gke_sourcegraph-dev_us-central1-f_cloud -f -
+kubectl apply --dry-run --validate --recursive -f configure/ --context=gke_sourcegraph-dev_us-central1-f_cloud
 
 .buildkite/verify-label.sh
